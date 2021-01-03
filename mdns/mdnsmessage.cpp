af0eb4a325ec6f3088a0de4157a843d4928cfd0d
Mathias Hasselmann <mathias.hasselmann@meiller.com>
2021-01-03 17:47:19 +0100

    Add proper DNS message parser

---
+++ mdns/mdnsmessage.cpp
@@ -0,0 +1,395 @@
+#include "mdnsmessage.h"
+
+#include <QHostAddress>
+#include <QLoggingCategory>
+
+#include <QtEndian>
+
+namespace MDNS {
+
+namespace {
+
+Q_LOGGING_CATEGORY(lcMessage, "mdns.message")
+
+auto readUInt16(const char *p)
+{
+    return qFromBigEndian<quint16>(p);
+}
+
+void writeUInt16(char *p, quint16 value)
+{
+    qToBigEndian(value, p);
+}
+
+auto readUInt32(const char *p)
+{
+    return qFromBigEndian<quint32>(p);
+}
+
+auto makeByteArray(QList<Label> labels)
+{
+    QByteArray data;
+
+    for (const auto &l: labels)
+        data.append(l.data());
+
+    return data;
+}
+
+auto makeLabelList(QList<QByteArray> strings)
+{
+    QList<Label> labels;
+
+    for (const auto &s: strings) {
+        Q_ASSERT(s.length() > 0);
+        Q_ASSERT(s.length() < 64);
+        labels.append(Label{static_cast<char>(s.length()) + s, 0});
+    }
+
+    labels.append(Label{QByteArray{"", 1}, 0});
+
+    return labels;
+}
+
+} // namespace
+
+Entry::Entry(QByteArray data, int offset)
+    : m_data{std::move(data)}
+    , m_offset{offset}
+{}
+
+quint16 Entry::u16(int offset) const
+{
+    return readUInt16(m_data.constData() + offset);
+}
+
+void Entry::setU16(int offset, quint16 value)
+{
+    writeUInt16(m_data.data() + offset, value);
+}
+
+quint32 Entry::u32(int offset) const
+{
+    return readUInt32(m_data.constData() + offset);
+}
+
+QByteArray Label::labelText() const
+{
+    return data().mid(offset() + 1, labelLength());
+}
+
+int Label::size() const
+{
+    if (isLabel())
+        return 1 + u8(offset());
+    if (isPointer())
+        return 2;
+
+    return 1;
+}
+
+Name::Name(QList<QByteArray> labels)
+    : Entry{makeByteArray(makeLabelList(std::move(labels))), 0}
+{}
+
+QByteArray Name::toByteArray() const
+{
+    QByteArray name;
+
+    for (int i = 0;; ++i) {
+        const auto l = label(i);
+
+        if (l.isPointer()) {
+            name.append(Name{data(), l.pointer()}.toByteArray());
+            break;
+        }
+
+        if (l.labelLength() == 0)
+            break;
+
+        name.append(l.labelText());
+        name.append('.');
+    }
+
+    return name;
+}
+
+Label Name::label(int i) const
+{
+    return Label{data(), i > 0 ? label(i - 1).nextOffset() : offset()};
+}
+
+int Name::size() const
+{
+    int size = 0;
+
+    for (int i = 0;; ++i) {
+        const auto l = label(i);
+        size += l.size();
+
+        if (l.labelLength() == 0) // empty label, or pointer
+            break;
+    }
+
+    return size;
+}
+
+Question::Question(QByteArray name, Message::Type type, Message::NetworkClass networkClass, bool flush) noexcept
+    : Entry{Name{name.split('.')}.data() + QByteArray{4, Qt::Uninitialized}, 0}
+{
+    setU16(fieldsOffset() + TypeOffset, type);
+    setU16(fieldsOffset() + FlagsOffset, (networkClass & 0x7f) | (flush ? 0x80 : 0x00));
+}
+
+QHostAddress Resource::address() const
+{
+    if (type() == Message::A && dataSize() == 4)
+        return QHostAddress{u32(dataOffset())};
+    else if (type() == Message::AAAA && dataSize() == 16)
+        return QHostAddress{reinterpret_cast<const quint8 *>(data().constData() + dataOffset())};
+    else
+        return {};
+}
+
+Name Resource::pointer() const
+{
+    if (type() == Message::PTR && dataSize() > 0)
+        return Name{data(), dataOffset()};
+    else
+        return Name{};
+}
+
+QByteArray Resource::text() const
+{
+    if (type() == Message::TXT)
+        return {data().constData() + dataOffset(), dataSize()};
+    else
+        return {};
+}
+
+ServiceRecord Resource::service() const
+{
+    if (type() == Message::SRV && dataSize() >= 8)
+        return ServiceRecord{data(), dataOffset()};
+    else
+        return ServiceRecord{};
+}
+
+Question Message::question(int i) const
+{
+    if (i == 0) {
+        return Question{m_data, SizeOfFields};
+    } else if (i < 0) {
+        return question(questionCount() + i);
+    } else if (i < questionCount()) {
+        return Question{m_data, question(i - 1).nextOffset()};
+    } else {
+        return Question{};
+    }
+}
+
+Resource Message::answer(int i) const
+{
+    if (i == 0) {
+        if (questionCount() > 0)
+            return Resource{m_data, question(-1).nextOffset()};
+        else
+            return Resource{m_data, SizeOfFields};
+    } else if (i < 0) {
+        return answer(answerCount() + i);
+    } else if (i < answerCount()) {
+        return Resource{m_data, answer(i - 1).nextOffset()};
+    } else {
+        return Resource{};
+    }
+}
+
+Resource Message::authority(int i) const
+{
+    if (i == 0) {
+        if (answerCount() > 0)
+            return Resource{m_data, answer(-1).nextOffset()};
+        else if (questionCount() > 0)
+            return Resource{m_data, question(-1).nextOffset()};
+        else
+            return Resource{m_data, SizeOfFields};
+    } else if (i < 0) {
+        return authority(authorityCount() + i);
+    } else if (i < authorityCount()) {
+        return Resource{m_data, authority(i - 1).nextOffset()};
+    } else {
+        return Resource{};
+    }
+}
+
+Resource Message::additional(int i) const
+{
+    if (i == 0) {
+        if (authorityCount() > 0)
+            return Resource{m_data, authority(-1).nextOffset()};
+        else if (answerCount() > 0)
+            return Resource{m_data, answer(-1).nextOffset()};
+        else if (questionCount() > 0)
+            return Resource{m_data, question(-1).nextOffset()};
+        else
+            return Resource{m_data, SizeOfFields};
+    } else if (i < 0) {
+        return additional(additionalCount() + i);
+    } else if (i < additionalCount()) {
+        return Resource{m_data, additional(i - 1).nextOffset()};
+    } else {
+        return Resource{};
+    }
+}
+
+Message &Message::addQuestion(Question question)
+{
+    Q_ASSERT(answerCount() == 0);
+    Q_ASSERT(authorityCount() == 0);
+    Q_ASSERT(additionalCount() == 0);
+
+    m_data.append(question.data());
+    setU16(QuestionCountOffset, questionCount() + 1);
+
+    return *this;
+}
+
+quint16 Message::u16(int offset) const
+{
+    return readUInt16(m_data.constData() + offset);
+}
+
+void Message::setU16(int offset, quint16 value)
+{
+    writeUInt16(m_data.data() + offset, value);
+}
+
+} // namespace MDNS
+
+QDebug operator<<(QDebug debug, const MDNS::Message &message)
+{
+    const QDebugStateSaver saver{debug};
+
+    if (debug.verbosity() >= QDebug::DefaultVerbosity)
+        debug.nospace() << message.staticMetaObject.className();
+
+    debug.nospace().verbosity(QDebug::MinimumVerbosity)
+            << "(serial=" << message.serial()
+            << ", flags=" << Qt::hex << message.flags() << Qt::dec
+            << ", #questions=" << message.questionCount()
+            << ", #anwers=" << message.answerCount()
+            << ", #authorities=" << message.authorityCount()
+            << ", #additionals=" << message.additionalCount()
+            << "," << Qt::endl
+            << "  questions=(";
+
+    for (int i = 0; i < message.questionCount(); ++i) {
+        if (i > 0) debug << "," << Qt::endl << "             ";
+        debug << message.question(i);
+    }
+
+    debug << ")," << Qt::endl << "  answers=(";
+
+    for (int i = 0; i < message.answerCount(); ++i) {
+        if (i > 0) debug << "," << Qt::endl << "           ";
+        debug << message.answer(i);
+    }
+
+    debug << ")," << Qt::endl << "  authorities=(";
+
+    for (int i = 0; i < message.authorityCount(); ++i) {
+        if (i > 0) debug << "," << Qt::endl << "               ";
+        debug << message.authority(i);
+    }
+
+    debug << ")" << Qt::endl << "  additionals=(";
+
+    for (int i = 0; i < message.additionalCount(); ++i) {
+        if (i > 0) debug << "," << Qt::endl << "               ";
+        debug << message.additional(i);
+    }
+
+    debug << "))";
+    return debug;
+}
+
+QDebug operator<<(QDebug debug, const MDNS::Name &name)
+{
+    return debug << name.toByteArray();
+}
+
+QDebug operator<<(QDebug debug, const MDNS::Question &question)
+{
+    const QDebugStateSaver saver{debug};
+
+    if (debug.verbosity() >= QDebug::DefaultVerbosity)
+        debug.nospace() << question.staticMetaObject.className();
+
+    debug.nospace().verbosity(QDebug::MinimumVerbosity)
+            << "(name=" << question.name()
+            << ", type=" << question.type()
+            << ", class=" << question.networkClass()
+            << ", flush=" << question.flush();
+
+    if (debug.verbosity() > QDebug::DefaultVerbosity)
+        debug << ", offset=" << question.offset()
+              << ", size=" << question.size();
+
+    return debug << ")";
+}
+
+QDebug operator<<(QDebug debug, const MDNS::Resource &resource)
+{
+    const QDebugStateSaver saver{debug};
+
+    if (debug.verbosity() >= QDebug::DefaultVerbosity)
+        debug.nospace() << resource.staticMetaObject.className();
+
+    debug.nospace().verbosity(QDebug::MinimumVerbosity)
+            << "(name=" << resource.name()
+            << ", type=" << resource.type()
+            << ", class=" << resource.networkClass()
+            << ", flush=" << resource.flush()
+            << ", ttl=" << resource.timeToLife()
+            << ", dataSize=" << resource.dataSize();
+
+    switch (resource.type()) {
+    case MDNS::Message::A:
+    case MDNS::Message::AAAA:
+        debug << ", address=" << resource.address();
+        break;
+
+    case MDNS::Message::PTR:
+        debug << ", pointer=" << resource.pointer();
+        break;
+
+    case MDNS::Message::TXT:
+        debug << ", text=" << resource.text();
+        break;
+
+    case MDNS::Message::SRV:
+        debug << ", service=" << resource.service();
+        break;
+    }
+
+    if (debug.verbosity() > QDebug::DefaultVerbosity)
+        debug << ", offset=" << resource.offset()
+              << ", size=" << resource.size();
+
+    return debug << ")";
+}
+
+QDebug operator<<(QDebug debug, const MDNS::ServiceRecord &service)
+{
+    const QDebugStateSaver saver{debug};
+
+    if (debug.verbosity() >= QDebug::DefaultVerbosity)
+        debug.nospace() << service.staticMetaObject.className();
+
+    return debug.nospace().verbosity(QDebug::MinimumVerbosity)
+            << "(priority=" << service.priority()
+            << ", weight=" << service.weight()
+            << ", port=" << service.port()
+            << ", target=" << service.target()
+            << ")";
+}
---
+++ mdns/mdnsmessage.h
@@ -0,0 +1,262 @@
+#ifndef MDNS_MDNSMESSAGE_H
+#define MDNS_MDNSMESSAGE_H
+
+#include <QMetaType>
+#include <QByteArray>
+
+class QHostAddress;
+
+namespace MDNS {
+
+class Question;
+class Resource;
+
+class Entry
+{
+    Q_GADGET
+
+public:
+    Entry() noexcept  = default;
+    explicit Entry(QByteArray data, int offset);
+
+    int offset() const { return m_offset; }
+    auto data() const { return m_data; }
+
+protected:
+    quint8 u8(int offset) const { return m_data.at(offset); }
+    quint16 u16(int offset) const;
+    quint32 u32(int offset) const;
+
+    void setU16(int offset, quint16 value);
+
+private:
+    int m_offset = 0;
+    QByteArray m_data;
+};
+
+class Message
+{
+    Q_GADGET
+
+    enum FieldOffset {
+        SerialOffset = 0,
+        FlagsOffset = 2,
+        QuestionCountOffset = 4,
+        AnswerCountOffset = 6,
+        AuthorityCountOffset = 8,
+        AdditionalCountOffset = 10,
+        SizeOfFields = 12,
+    };
+
+public:
+    enum Type
+    {
+        A = 1,
+        AAAA = 28,
+        ANY = 255,
+        CNAME = 5,
+        NSEC = 47,
+        MX = 15,
+        NS = 2,
+        PTR = 12,
+        SRV = 33,
+        TXT = 16,
+    };
+
+    Q_ENUM(Type)
+
+    enum NetworkClass
+    {
+        IN = 1,
+    };
+
+    Q_ENUM(NetworkClass)
+
+    enum Flag
+    {
+        IsResponse = (1 << 15),
+        OperationCode = (15 << 11),
+        AuthoritativeAnswer = (1 << 10),
+        Truncated = (1 << 9),
+        RecursionDesired = (1 << 8),
+        RecursionAvailable = (1 << 7),
+        ResponseCode = (15 << 0),
+    };
+
+    Q_FLAG(Flag)
+    Q_DECLARE_FLAGS(Flags, Flag)
+
+    enum Operation
+    {
+        Query,
+        IQuery,
+        Status,
+    };
+
+    Q_ENUM(Operation)
+
+    enum Error
+    {
+        NoError,
+        FormatError,
+        ServerError,
+        NameError,
+        NotImplemented,
+        Refused,
+    };
+
+    Q_ENUM(Error)
+
+    Message() noexcept
+        : m_data{SizeOfFields, '\0'} {}
+    explicit Message(QByteArray data) noexcept
+        : m_data{std::move(data)} {}
+
+    int serial() const { return u16(SerialOffset); }
+    Flags flags() const { return Flags{u16(FlagsOffset)}; }
+    int questionCount() const { return u16(QuestionCountOffset); }
+    int answerCount() const { return u16(AnswerCountOffset); }
+    int authorityCount() const { return u16(AuthorityCountOffset); }
+    int additionalCount() const { return u16(AdditionalCountOffset); }
+
+    bool isQuery() const { return !flags().testFlag(Flag::IsResponse); }
+    bool isResponse() const { return flags().testFlag(Flag::IsResponse); }
+    Operation operation() const { return static_cast<Operation>((static_cast<int>(flags()) >> 11) & 15); }
+    Error error() const { return static_cast<Error>(static_cast<int>(flags()) & 15); }
+
+    Question question(int i) const;
+    Resource answer(int i) const;
+    Resource authority(int i) const;
+    Resource additional(int i) const;
+
+    Message &addQuestion(Question question);
+    Message &addAnswer(Resource resource);
+    Message &addAuthority(Resource resource);
+    Message &addAdditional(Resource resource);
+
+    auto data() const { return m_data; }
+
+private:
+    quint16 u16(int offset) const;
+    void setU16(int offset, quint16 value);
+
+    QByteArray m_data;
+};
+
+class Label : public Entry
+{
+    Q_GADGET
+
+public:
+    using Entry::Entry;
+
+    bool isLabel() const { return (u8(offset()) & 0xc0) == 0x00; }
+    int labelLength() const { return isLabel() ? u8(offset()) : 0; }
+    QByteArray labelText() const;
+
+    bool isPointer() const { return (u8(offset()) & 0xc0) == 0xc0; }
+    int pointer() const { return u16(offset()) & 0x3fff; }
+
+    int size() const;
+    int nextOffset() const { return offset() + size(); }
+};
+
+class Name : public Entry
+{
+    Q_GADGET
+
+public:
+    using Entry::Entry;
+
+    explicit Name(QList<QByteArray> labels);
+
+    QByteArray toByteArray() const;
+    Label label(int i) const;
+
+    int size() const;
+    int nextOffset() const { return offset() + size(); }
+};
+
+class Question : public Entry
+{
+    Q_GADGET
+
+    enum FieldOffset {
+        TypeOffset = 0,
+        FlagsOffset = 2,
+        SizeOfFields = 4,
+    };
+
+public:
+    using Entry::Entry;
+
+    Question(QByteArray name, Message::Type type, Message::NetworkClass networkClass, bool flush = false) noexcept;
+    Question(QByteArray name, Message::Type type, bool flush = false) noexcept
+        : Question{std::move(name), type, Message::IN, flush} {}
+
+    auto name() const { return Name{data(), offset()}; }
+    auto type() const { return static_cast<Message::Type>(u16(fieldsOffset() + TypeOffset)); }
+    auto networkClass() const { return static_cast<Message::NetworkClass>(u16(fieldsOffset() + FlagsOffset) & 0x7ff); }
+    bool flush() const { return u16(fieldsOffset() + FlagsOffset) & 0x8000; }
+    int size() const { return name().size() + SizeOfFields; }
+
+    int fieldsOffset() const { return offset() + name().size(); }
+    int nextOffset() const { return offset() + size(); }
+};
+
+class ServiceRecord : public Entry
+{
+    Q_GADGET
+
+public:
+    using Entry::Entry;
+
+    int priority() const { return u16(offset()); }
+    int weight() const { return u16(offset() + 2); }
+    int port() const { return u16(offset() + 4); }
+    auto target() const { return Name{data(), offset() + 6}; }
+};
+
+class Resource : public Entry
+{
+    Q_GADGET
+
+    enum FieldOffset {
+        TypeOffset = 0,
+        FlagsOffset = 2,
+        TimeToLifeOffset = 4,
+        DataSizeOffset = 8,
+        SizeOfFields = 10,
+    };
+
+public:
+    using Entry::Entry;
+
+    auto name() const { return Name{data(), offset()}; }
+    auto type() const { return static_cast<Message::Type>(u16(fieldsOffset() + TypeOffset)); }
+    auto networkClass() const { return static_cast<Message::NetworkClass>(u16(fieldsOffset() + FlagsOffset) & 0x7ff); }
+    bool flush() const { return u16(fieldsOffset() + FlagsOffset) & 0x8000; }
+    qint64 timeToLife() const { return u32(fieldsOffset() + TimeToLifeOffset); }
+    int dataSize() const { return u16(fieldsOffset() + DataSizeOffset); }
+    int size() const { return name().size() + SizeOfFields + dataSize(); }
+
+    QHostAddress address() const;
+    Name pointer() const;
+    QByteArray text() const;
+    ServiceRecord service() const;
+
+    int fieldsOffset() const { return offset() + name().size(); }
+    int dataOffset() const { return fieldsOffset() + SizeOfFields; }
+    int nextOffset() const { return offset() + size(); }
+};
+
+} // namespace MDNS
+
+QDebug operator<<(QDebug, const MDNS::Label &);
+QDebug operator<<(QDebug, const MDNS::Message &);
+QDebug operator<<(QDebug, const MDNS::Name &);
+QDebug operator<<(QDebug, const MDNS::Question &);
+QDebug operator<<(QDebug, const MDNS::Resource &);
+QDebug operator<<(QDebug, const MDNS::ServiceRecord &);
+
+#endif // MDNS_MDNSMESSAGE_H
